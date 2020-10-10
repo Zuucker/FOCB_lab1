ee5a178285d44ef7d29a274272d16e06067d848d
Zuucker <kamigie504@student.polsl.pl>
2020-10-10 11:53:11 +0200

    even/odd number printing

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -8,5 +8,12 @@ int main()
 	cout << "give me a number pls ";
 	cin >> myNumb;
 	cout << "You gave me a " << myNumb << "!" << endl;
+	if (myNumb % 2 == 0) {
+
+		cout << "And the number is even!" << endl;
+	}
+	else{
+		cout << "And the number is odd!" << endl;
+	}
 	return 0;
 }
