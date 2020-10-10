b6c32146439d1cb6f0ffef3b1b7c8139a25d53b5
Zuucker <kamigie504@student.polsl.pl>
2020-10-10 12:55:47 +0200

    convert while loop into for loop

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -4,17 +4,26 @@ using namespace std;
 int main()
 {
 	int totalMoney;
+	int banknoteValue;
 
 	cout << "Please introduce an amount of money: " << endl;
 	cin >> totalMoney;
+	cout << "Please introduce the value of the banknote: " << endl;
+	cin >> banknoteValue;
 
 	int banknotes=0;
-
+/*
 	while (totalMoney >= 50)
 	{
 		totalMoney -= 50;
 		banknotes++;
 
+	}
+	*/
+
+	for ( banknotes = 0; totalMoney>= banknotes ; banknotes++)
+	{
+		totalMoney = totalMoney -banknoteValue;
 	}
 	cout << "number of banknotes: " << banknotes << endl;
 
