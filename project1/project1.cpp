1623606d3eae268004c91fe4b621b90fb96562db
Zuucker <kamigie504@student.polsl.pl>
2020-10-10 12:07:49 +0200

    ax+b=c

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -4,6 +4,27 @@ using namespace std;
 int main()
 {
 	int myNumb;
+	float a,b,c;
+
+	cout << "please introduce a " << endl;
+	cin >> a;
+	cout << "please introduce b " << endl;
+	cin >> b;
+	cout << "please introduce c " << endl;
+	cin >> c;
+
+	float x = (c - b) / a;
+
+	cout << "The x is " << x << "! " << "More stuff!" << endl;
+
+
+
+
+
+
+
+
+	/*
 	cout << "Hello World"<<endl;
 	cout << "give me a number pls ";
 	cin >> myNumb;
@@ -15,5 +36,6 @@ int main()
 	else{
 		cout << "And the number is odd!" << endl;
 	}
+	*/
 	return 0;
 }
